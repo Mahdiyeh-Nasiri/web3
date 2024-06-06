5ac3c292405dfc8bf1698af6c196cb8322e5f28d
Mahdiyeh-Nasiri <148885449+Mahdiyeh-Nasiri@users.noreply.github.com>
2024-06-06 15:25:15 +0330

    یی

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-//navigation
+
+    //navigation
         const NAVIGATION = document.querySelector(".menu");
         const ITEM_PRODUCT1 = document.querySelectorAll("li")[5];
         const ITEM_PRODUCT2 = document.querySelectorAll("li")[7];
@@ -9,7 +10,9 @@
         const ICON_NAV1 = document.querySelector(".icon-nav1");
         const RETURN = document.querySelector(".return");
         const RETURN_SUPPORT = document.querySelector(".return-support");
-        const SUBLIST_SUPPORT = document.querySelectorAll(".sublist-support");        
+        const SUBLIST_SUPPORT = document.querySelectorAll(".sublist-support");
+        const MODAL_CONTAINER = document.querySelector(".modal-container"); 
+        const ICON_MODAL = document.querySelector(".icon-modal");    
         let key;
         let key_nav2;
 
@@ -18,6 +21,7 @@
     if (screen_width >= 576) {
         SUBMENU_PRODUCT.classList.add("hidden");
         SUBMENU_SUPPORT.classList.add("hidden");
+
         ITEM_PRODUCT1.addEventListener("mouseover", function () {
             clearTimeout(key);
             SUBMENU_PRODUCT.classList.remove("hidden");
@@ -29,7 +33,7 @@
             SUBMENU_PRODUCT.classList.remove("show-submenu");
             key = setTimeout(function () {
                 SUBMENU_PRODUCT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
         SUBMENU_PRODUCT.addEventListener("mouseover", function () {
             clearTimeout(key);
@@ -42,9 +46,9 @@
             SUBMENU_PRODUCT.classList.remove("show-submenu");
             key = setTimeout(function () {
                 SUBMENU_PRODUCT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
-                ITEM_PRODUCT2.addEventListener("mouseover", function () {
+            ITEM_PRODUCT2.addEventListener("mouseover", function () {
             clearTimeout(key_nav2);
             SUBMENU_SUPPORT.classList.remove("hidden");
             setTimeout(function () {
@@ -55,7 +59,7 @@
             SUBMENU_SUPPORT.classList.remove("show-submenu");
             key_nav2 = setTimeout(function () {
                 SUBMENU_SUPPORT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
         SUBMENU_SUPPORT.addEventListener("mouseover", function () {
             clearTimeout(key_nav2);
@@ -68,13 +72,19 @@
             SUBMENU_SUPPORT.classList.remove("show-submenu");
             key_nav2 = setTimeout(function () {
                 SUBMENU_SUPPORT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
     }
     else {
         NAVIGATION.classList.add("hidden");
         SUBMENU_PRODUCT.classList.add("hidden");
         SUBMENU_SUPPORT.classList.add("hidden");
+
+        MENU.addEventListener("click", function () {
+            MODAL_CONTAINER.classList.remove("hidden");
+            setTimeout(function () { MODAL_CONTAINER.classList.add("opacity-100"); }, 1);
+        });
+
         MENU.addEventListener("click", function () {
             NAVIGATION.classList.toggle("hidden");
             NAVIGATION.classList.toggle("show-submenu");        
@@ -95,6 +105,13 @@
             SUBMENU_SUPPORT.classList.remove("show-submenu");
            SUBMENU_SUPPORT.classList.toggle("hidden");
         });
+        ICON_MODAL.addEventListener("click", function () {
+            MODAL_CONTAINER.classList.add("hidden");
+            NAVIGATION.classList.add("hidden");
+            SUBMENU_PRODUCT.classList.add("hidden");
+            SUBMENU_SUPPORT.classList.add("hidden");
+            setTimeout(function () { MODAL_CONTAINER.classList.remove("opacity-100"); }, 1);
+        });
 
     }
 });
@@ -103,6 +120,7 @@ window.addEventListener("load", function () {
     if (screen_width >= 576) {
         SUBMENU_PRODUCT.classList.add("hidden");
         SUBMENU_SUPPORT.classList.add("hidden");
+
         ITEM_PRODUCT1.addEventListener("mouseover", function () {
             clearTimeout(key);
             SUBMENU_PRODUCT.classList.remove("hidden");
@@ -114,7 +132,7 @@ window.addEventListener("load", function () {
             SUBMENU_PRODUCT.classList.remove("show-submenu");
             key = setTimeout(function () {
                 SUBMENU_PRODUCT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
         SUBMENU_PRODUCT.addEventListener("mouseover", function () {
             clearTimeout(key);
@@ -127,9 +145,9 @@ window.addEventListener("load", function () {
             SUBMENU_PRODUCT.classList.remove("show-submenu");
             key = setTimeout(function () {
                 SUBMENU_PRODUCT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
-            ITEM_PRODUCT2.addEventListener("mouseover", function () {
+        ITEM_PRODUCT2.addEventListener("mouseover", function () {
             clearTimeout(key_nav2);
             SUBMENU_SUPPORT.classList.remove("hidden");
             setTimeout(function () {
@@ -140,7 +158,7 @@ window.addEventListener("load", function () {
             SUBMENU_SUPPORT.classList.remove("show-submenu");
             key_nav2 = setTimeout(function () {
                 SUBMENU_SUPPORT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
         SUBMENU_SUPPORT.addEventListener("mouseover", function () {
             clearTimeout(key_nav2);
@@ -153,13 +171,18 @@ window.addEventListener("load", function () {
             SUBMENU_SUPPORT.classList.remove("show-submenu");
             key_nav2 = setTimeout(function () {
                 SUBMENU_SUPPORT.classList.add("hidden");
-            }, 401);
+            }, 40);
         });
     }
     else {
         NAVIGATION.classList.add("hidden");
         SUBMENU_PRODUCT.classList.add("hidden");
-              SUBMENU_SUPPORT.classList.add("hidden");
+        SUBMENU_SUPPORT.classList.add("hidden");
+        
+        MENU.addEventListener("click", function () {
+            MODAL_CONTAINER.classList.remove("hidden");
+            setTimeout(function () { MODAL_CONTAINER.classList.add("opacity-100"); }, 1);
+        });
         MENU.addEventListener("click", function () {
             NAVIGATION.classList.toggle("hidden");
             NAVIGATION.classList.toggle("show-submenu");
@@ -179,11 +202,18 @@ window.addEventListener("load", function () {
         RETURN_SUPPORT.addEventListener("click", function () {
             SUBMENU_SUPPORT.classList.remove("show-submenu");
            SUBMENU_SUPPORT.classList.toggle("hidden");
-        });
+        })
 
-    }
+    ICON_MODAL.addEventListener("click", function () {
+    MODAL_CONTAINER.classList.add("hidden");
+    NAVIGATION.classList.add("hidden");
+    SUBMENU_PRODUCT.classList.add("hidden");
+    SUBMENU_SUPPORT.classList.add("hidden");
+    setTimeout(function () { MODAL_CONTAINER.classList.remove("opacity-100"); }, 1);
 });
 
+    };
+});
 //first slider
     const SLIDER_container = document.querySelector('.slider-container');
         const IMAGE_SLIDER = document.querySelectorAll('.imageslide');
@@ -191,63 +221,61 @@ window.addEventListener("load", function () {
         let index = IMAGE_SLIDER.length - 1;
         const rectangles = document.querySelectorAll('.rect');
         let autoSlideInterval;
+window.addEventListener("load", function () {
+    function nextSlide1() {
+        index = (index - 1 + IMAGE_SLIDER.length) % IMAGE_SLIDER.length;
+        SLIDER_container.style.transition = 'transform 0.5s ease-in-out';
+        SLIDER_container.style.transform = `translateX(${slideWidth1 * index}px)`;
+        fillRectangle(index);
+    }
 
-        function nextSlide1() {
-            index = (index - 1 + IMAGE_SLIDER.length) % IMAGE_SLIDER.length;
+    function prevSlide1() {
+        index = (index + 1 + IMAGE_SLIDER.length) % IMAGE_SLIDER.length;
+        SLIDER_container.style.transition = 'transform 0.5s ease-in-out';
+        SLIDER_container.style.transform = `translateX(${slideWidth1 * index}px)`;
+        fillRectangle(index);
+    }
+
+    function fillRectangle(index) {
+        let reversedRectangles = Array.from(rectangles).reverse();
+        reversedRectangles.forEach((rectangle, i) => {
+            if (i === index) {
+                rectangle.style.transition = '3s ease-out';
+                rectangle.style.backgroundPosition = 'right';
+            } else {
+                rectangle.style.backgroundPosition = null;
+                rectangle.style.transition = null;
+            }
+        });
+    }
+    rectangles.forEach((rectangle, i) => {
+        rectangle.addEventListener('click', () => {
+            index = rectangles.length - 1 - i;
             SLIDER_container.style.transition = 'transform 0.5s ease-in-out';
             SLIDER_container.style.transform = `translateX(${slideWidth1 * index}px)`;
             fillRectangle(index);
-        }
+            IMAGE_SLIDER.style.transform = `translateX(${slideWidth1 * index}px)`;
+        });
+    });
+    
+    document.querySelector('.icon1').addEventListener('click', nextSlide1);
+    document.querySelector('.icon2').addEventListener('click', prevSlide1);
 
-        function prevSlide1() {
-            index = (index + 1 + IMAGE_SLIDER.length) % IMAGE_SLIDER.length;
-            SLIDER_container.style.transition = 'transform 0.5s ease-in-out';
-            SLIDER_container.style.transform = `translateX(${slideWidth1 * index}px)`;
-            fillRectangle(index);
-        }
-
-        function fillRectangle(index) {
-            let reversedRectangles = Array.from(rectangles).reverse(); // Reverse the order of rectangles
-            reversedRectangles.forEach((rectangle, i) => {
-                if (i === index) {
-                    rectangle.style.transition = '3s ease-out';
-                    rectangle.style.backgroundPosition = 'right'; // Apply fill to the rightmost rectangle first
-                } else {
-                    rectangle.style.backgroundPosition = null;
-                    rectangle.style.transition = null;
-                }
-            });
-        }
-            rectangles.forEach((rectangle, i) => {
-                rectangle.addEventListener('click', () => {
-                    index = rectangles.length - 1 - i; // Calculate the corresponding index of the image
-                    SLIDER_container.style.transition = 'transform 0.5s ease-in-out';
-                    SLIDER_container.style.transform = `translateX(${slideWidth1 * index}px)`;
-                    fillRectangle(index);
-                    // Display the corresponding image
-                    IMAGE_SLIDER.style.transform = `translateX(${slideWidth1 * index}px)`;
-                });
-            });
-        // Add event listeners to ICON_LEFT and ICON_RIGHT
-        document.querySelector('.icon1').addEventListener('click', nextSlide1);
-        document.querySelector('.icon2').addEventListener('click', prevSlide1);
-
-// Automatically switch to the next slide every 3 seconds
         
-                /*function startSlide1() {
-                    autoSlideInterval = setInterval(nextSlide1, 3000);
-                }
+    /*function startSlide1() {
+        autoSlideInterval = setInterval(nextSlide1, 3000);
+    }
 
-                function stopSlide1() {
-                    clearInterval(autoSlideInterval);
-                }
+    function stopSlide1() {
+        clearInterval(autoSlideInterval);
+    }
 
-                startSlide1();
+    startSlide1();
 
-                // Add event listeners to start and stop slider on mouseenter and mouseleave
-                SLIDER_container.addEventListener('mouseenter', stopSlide1);
-                SLIDER_container.addEventListener('mouseleave', startSlide1);*/
-setInterval(nextSlide1, 3000);
+    SLIDER_container.addEventListener('mouseenter', stopSlide1);
+    SLIDER_container.addEventListener('mouseleave', startSlide1);*/
+    setInterval(nextSlide1, 3000);
+});
 //imagecontanier hover
             const images = document.querySelectorAll('.pic');
 
@@ -275,47 +303,48 @@ setInterval(nextSlide1, 3000);
             const slides = document.querySelectorAll('.pic3');
             let counter = slides.length - 1; // Start from the last slide
             let key2;
+window.addEventListener("load", function () {
+    function updateSlider() {
+        let slideWidth = slides[0].clientWidth;
+        slider.style.transform = `translateX(${slideWidth * counter}px)`;
+    }
 
-            function updateSlider() {
-                let slideWidth = slides[0].clientWidth;
-                slider.style.transform = `translateX(${slideWidth * counter}px)`;
-            }
+    function nextSlide() {
+        if (counter < slides.length - 1) {
+            counter++;
+        } else {
+            counter = 0;
+        }
+        updateSlider();
+    }
 
-            function nextSlide() {
-                if (counter < slides.length - 1) {
-                    counter++;
-                } else {
-                    counter = 0;
-                }
-                updateSlider();
-            }
+    function prevSlide() {
+        if (counter > 0) {
+            counter--;
+        } else {
+            counter = slides.length - 1;
+        }
+        updateSlider();
+    }
 
-            function prevSlide() {
-                if (counter > 0) {
-                    counter--;
-                } else {
-                    counter = slides.length - 1;
-                }
-                updateSlider();
-            }
+    document.querySelector('.icon-right').addEventListener("click", prevSlide);
+    document.querySelector('.icon-left').addEventListener("click", nextSlide);
 
-            document.querySelector('.icon-right').addEventListener("click", prevSlide);
-            document.querySelector('.icon-left').addEventListener("click", nextSlide);
+    function startSlide() {
+        key2 = setInterval(prevSlide, 3000); // Adjust to call prevSlide to move towards the first slide
+    }
 
-            function startSlide() {
-                key2 = setInterval(prevSlide, 3000); // Adjust to call prevSlide to move towards the first slide
-            }
+    function stopSlide() {
+        clearInterval(key2);
+    }
 
-            function stopSlide() {
-                clearInterval(key2);
-            }
+    startSlide();
 
-            startSlide();
+    slider.addEventListener('mouseenter', stopSlide);
+    slider.addEventListener('mouseleave', startSlide);
 
-            slider.addEventListener('mouseenter', stopSlide);
-            slider.addEventListener('mouseleave', startSlide);
-
-            updateSlider();
+    updateSlider();
+});
 
 //footer in 300-575
        const HEADFOOTER = document.querySelectorAll(".headfooter");
@@ -402,3 +431,4 @@ setInterval(nextSlide1, 3000);
             SUB[0].classList.remove("open");
             SUB[7].classList.toggle("open");
                 });
+                
